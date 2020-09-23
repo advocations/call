80b82e578d0e0d12b3a8b13a01fa894059b29bab
advocations <lindsey@advocations.org>
2020-09-23 11:33:36 -0400

    Update pageBottom.js

--- custom/pageBottom.js
+++ custom/pageBottom.js
@@ -6,7 +6,7 @@ document.write('<img src="custom/logo.jpg" alt="Twilio"/>');
 document.write('<div id="bottomContent">');
 document.write('<div style="padding-bottom: 10px;"><a href="https://github.com/tigerfarm/owlcc" style="color: rgba(255,255,255,.65);">GitHub project</a></div>');
 document.write('<a href="https://twilio.learnupon.com/store" style="color: rgba(255,255,255,.65);">Training</a>');
-document.write(' | <a href="https://www.twilio.com/support-plans" style="color: rgba(255,255,255,.65);">Support</a>');
+document.write(' | <a href="https://www.twilio.com/support-plans" style="color: rgba(255,255,255,.65);">Support Help</a>');
 document.write(' | <a href="https://www.twilio.com/company" style="color: rgba(255,255,255,.65);">About Twilio</a>');
 document.write('</div>');
 
